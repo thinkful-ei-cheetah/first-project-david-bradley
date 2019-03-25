8ecc821b5459bb0b14815b9836ba892d423bc756
David Nordeen <davidnordeen@gmail.com>
2019-03-25 12:45:12 -0700

    task 6-8

--- functionDrills1.js
+++ functionDrills1.js
@@ -1,12 +1,35 @@
-function getYearOfBirth (age) {
-    return 2019 - age
+function getYearOfBirth(age) {
+    if (age < 0) {
+        throw new Error("Age can not be negative");
+    } else {
+        return 2019 - age
+    }
+}
+
+// try {
+//     const greeting1 = createGreeting('Mike', -15);
+// }
+// catch {
+//     console.log("Age can not be negative")
+// }
+
+function createGreeting(name, age) {
+    if (!age) {
+        alert('Arguments not valid')
+    } else if (typeof age !== number) {
+        throw new Error("Age must be a number")
+    } else if (!name) {
+        alert('Arguments not valid')
+    } else if (typeof name !== string) {
+        throw new Error("Name must be a word")
+    }
+
+    const yearOfBirth = 2019 - age;
+    return ("Hi my name is " + name + " and I'm " + age + " years old. I was born in " + yearOfBirth + ".");
 }
 
 
-function createGreeting (name, age){
-    const yearOfBirth = 2019 - age;
-    return ("Hi my name is " + name + " and I'm " + age + " years old. I was born in " + yearOfBirth + ".");
-    }
+
 
 const greeting1 = createGreeting('David', 24);
 console.log(greeting1);
