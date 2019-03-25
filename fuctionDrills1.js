c1471773c209c07bf27c7470ae3443b19aeaa220
David Nordeen <davidnordeen@gmail.com>
2019-03-25 11:55:36 -0700

    task 3

--- fuctionDrills1.js
+++ fuctionDrills1.js
@@ -1,8 +1,9 @@
 function createGreeting {
-    console.log ("Hi my name is Chris and I'm 24 years old");
+    return ("Hi my name is Chris and I'm 24 years old");
 
 }
 
-createGreeting();
+const greeting1 = createGreeting();
+console.log(greeting1);
 
 
