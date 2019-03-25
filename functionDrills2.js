65f6edb0bedd6b1b7dfb88c8443feb6f89603643
Brad Schilit <bradleyschilit@mac.com>
2019-03-25 17:29:54 -0400

    figure it out

--- functionDrills2.js
+++ functionDrills2.js
@@ -46,3 +46,44 @@ console.log(decode('craft block argon meter bells brown croon droop'));
 // b	            3
 // c	            4
 // d	            5
+
+
+function howManyDays (month, leapYear) {
+    if (month = 'January') {
+        let numberOfDays = 31
+    } else if (month == 'March') {
+        let numberOfDays = 31
+    } else if (month == 'April') {
+        let numberOfDays = 30
+    } else if (month == 'May') {
+        let numberOfDays = 31
+    } else if (month == 'June') {
+        let numberOfDays = 30
+    } else if (month == 'July') {
+        let numberOfDays = 31
+    } else if (month == 'August') {
+        let numberOfDays = 31
+    } else if (month == 'September') {
+        let numberOfDays = 30
+    } else if (month == 'October') {
+        let numberOfDays = 31
+    } else if (month == 'November') {
+        let numberOfDays = 30
+    } else if (month == 'December') {
+        let numberOfDays = 31
+    } else if (month == 'February') {
+        if (year % 400 == 0) {
+            let numberOfDays = 29
+        } else if (year % 100 == 0) {
+            let numberOfDays = 28
+        } else if (year % 4 == 0) {
+            let numberOfDays = 29
+        } else {
+            let numberOfDays = 28
+        }
+    } else {
+        return 'Must provide a valid month.'
+    }
+    return `${month} has ${numberOfDays} days.`
+}
+console.log (howManyDays(January, 2018))
