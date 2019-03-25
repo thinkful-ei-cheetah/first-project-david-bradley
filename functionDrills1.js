2e9d33a1a62ab5bc92db7453dea2f16ea620ee41
Brad Schilit <bradleyschilit@mac.com>
2019-03-25 15:12:13 -0400

    task6

--- functionDrills1.js
+++ functionDrills1.js
@@ -1,3 +1,8 @@
+function getYearOfBirth (age) {
+    return 2019 - age
+}
+
+
 function createGreeting (name, age){
     const yearOfBirth = 2019 - age;
     return ("Hi my name is " + name + " and I'm " + age + " years old. I was born in " + yearOfBirth + ".");
