ef5fc825d6d20720ed9945a19736822f71b2d788
David Nordeen <davidnordeen@gmail.com>
2019-03-25 12:08:18 -0700

    task 5

--- fuctionDrills1.js
+++
@@ -1,9 +0,0 @@
-function createGreeting (name, age){
-    return ("Hi my name is " + name + " and I'm " + age + " years old");
-
-}
-
-const greeting1 = createGreeting();
-console.log(greeting1);
-
-
---
+++ functionDrills1.js
@@ -0,0 +1,10 @@
+function createGreeting (name, age){
+    const yearOfBirth = 2019 - age;
+    return ("Hi my name is " + name + " and I'm " + age + " years old. I was born in " + yearOfBirth + ".");
+    }
+
+const greeting1 = createGreeting('David', 24);
+console.log(greeting1);
+
+
+
