126d5ceaee537629c02a1acef1964b984af64054
Brad Schilit <bradleyschilit@mac.com>
2019-03-25 15:58:13 -0400

    final

--- functionDrills1.js
+++ functionDrills1.js
@@ -1,35 +1,35 @@
 function getYearOfBirth(age) {
-    if (age < 0) {
-        throw new Error("Age can not be negative");
-    } else {
-        return 2019 - age
-    }
+    return 2019 - age
 }
-
-// try {
-//     const greeting1 = createGreeting('Mike', -15);
-// }
-// catch {
-//     console.log("Age can not be negative")
-// }
 
 function createGreeting(name, age) {
-    if (!age) {
-        alert('Arguments not valid')
-    } else if (typeof age !== number) {
-        throw new Error("Age must be a number")
-    } else if (!name) {
-        alert('Arguments not valid')
-    } else if (typeof name !== string) {
-        throw new Error("Name must be a word")
-    }
-
-    const yearOfBirth = 2019 - age;
-    return ("Hi my name is " + name + " and I'm " + age + " years old. I was born in " + yearOfBirth + ".");
+    if (name === undefined || age === undefined) {
+      throw new Error('Arguments not valid.');
 }
 
+if (age < 0) {
+    throw new Error('Age cannot be negative.');
+  }
 
+  if (typeof age !== 'number') {
+    throw new TypeError('Age must be a number');
+  }
+    
+    const yearOfBirth = getYearOfBirth(age);
+    return ("Hi my name is " + name + " and I'm " + age + " years old. I was born in " + yearOfBirth + ".");
 
+    //     return `
+//     Hi, my name is ${name} and I'm ${age} years old.
+//     I was born in ${yob}.
+//   `;
+
+}
+try {
+    const greeting1 = createGreeting('Mike', -15);
+    console.log (greeting1)
+} catch (e) {
+    console.error(e.message)
+}
 
 const greeting1 = createGreeting('David', 24);
 console.log(greeting1);
